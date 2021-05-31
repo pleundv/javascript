09efd4ac75d4b4a7f5d6d59fb33013f6f10e718e
pleundv <84378584+pleundv@users.noreply.github.com>
2021-05-31 18:02:43 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Hello Winc Academy")
+
+let name = 'pleun';
+console.log(name)
+
+let gedoe = 4 + 4
+console.log(gedoe)
+
+let niet = '4' + '4';
+console.log(niet)
+
+const ab = 6 + 5
+console.log(ab)
+
+const abc = 6 - 5
+console.log(abc)
+
+const abcd = 6 * 6
+console.log(abcd)
+
+const abcde = 6 / 6
+console.log(abcde)
+
+const abcdef = 6 % 4
+console.log(abcdef)
+
+let leeftijd = 50
+console.log(leeftijd)
